b711d5efd986d6e34a9779dff7137ff9162d8ab4
ZBound <mendietalisa638@gmail.com>
2024-02-17 12:37:14 +0800

    提交

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -46,12 +46,11 @@ export default hopeTheme({
     },
       
 docsearch: {
-	[
   appid: 'OOBSL6JRTO',
   apiKey: '1de3a192051c090c599a2a789a6fd96d',
   indexName: 'touhou',
-	],
-      },
+},
+
 
 
 
